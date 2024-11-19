372e8c280d2bb099677d2573513fa997ecd1fda5
Lisa Zeitlhofer <lisazeitlhofer.lz@gmail.com>
2024-11-19 20:23:02 +0100

    fix cors error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        https: false,
+        host: "chirper.test", // Your custom domain
+        cors: {
+            origin: "http://127.0.0.1:8000", // Laravel's origin
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            credentials: true, // Allow credentials if needed
+        },
+        port: 5173, // Ensure the port matches the VITE_DEV_SERVER_URL
+    },
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
